32db21ff5786e38d6e8b798d116e23332bcff7fb
jcornish <jcornish@vmware.com>
2021-12-06 10:07:25 -0700

    Uggly but working recursive site exporter

--- hack/harvest-sites-from-brain.sh
+++ hack/harvest-sites-from-brain.sh
@@ -21,11 +21,12 @@ HARVEST_MANIFEST_FILE="${REPO_ROOT}/_unzipped_export/${HARVEST_KEY}.md"
 
 TREE_DEF_FILES=$(extractTreesFromFile "${HARVEST_MANIFEST_FILE}")
 
-#echo "${TREE_DEF_FILES}" \
-#  | xargs -I {} -L1 mkdir -p "${REPO_ROOT}/_sites/{}"
-#echo "${TREE_DEF_FILES}" \
-#  | xargs -I {} -L1 cp "${REPO_ROOT}/_unzipped_export/{}.md" "${REPO_ROOT}/_sites/{}/index.md"
+echo "${TREE_DEF_FILES}" \
+  | xargs -I {} -L1 mkdir -p "${REPO_ROOT}/_sites/{}"
+echo "${TREE_DEF_FILES}" \
+  | xargs -I {} -L1 cp "${REPO_ROOT}/_unzipped_export/{}.md" "${REPO_ROOT}/_sites/{}/index.md"
 
 # move all referenced trees from def files
+# no I don't like this but it works
 echo "${TREE_DEF_FILES}" |
-  xargs -I {site} -L1 bash -c 'extractTreesFromFile "${REPO_ROOT}/_unzipped_export/{site}.md" | xargs -I {tree} -L1 cp "${REPO_ROOT}/_unzipped_export/{tree}.md" "${REPO_ROOT}/_sites/{site}/"'
+  xargs -I {} -L1 bash -c 'extractTreesFromFile "${REPO_ROOT}/_unzipped_export/{}.md" | xargs -I :: -L1 cp "${REPO_ROOT}/_unzipped_export/::.md" "${REPO_ROOT}/_sites/{}/"'
